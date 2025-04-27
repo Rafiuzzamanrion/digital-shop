8e2266622a7af22ad65848a39ee047eb75f7442c
Rafiuzzaman Rion <rafiujjaman5683@gmail.com>
2025-04-27 19:48:39 +0600

    scrollable nav added

---
+++ app/contactUs/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+	return (
+		<div className={'h-screen '}>
+			<h1 className={'text-4xl'}>from contact us page</h1>
+		</div>
+	);
+};
+
+export default Page;
